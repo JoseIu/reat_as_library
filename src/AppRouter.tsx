67eb7222d931dc1a15bcac2af74da370a9d94fb3
Joselu <boxixz0991@gmail.com>
2025-04-12 23:14:29 +0200

    feat: enhance AppRouter with lazy-loaded chat routes and auth subroutes for improved navigation and performance

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,11 +1,35 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 import AuthLayout from "./auth/layout/AuthLayout";
+import { LoginPage } from "./auth/pages/LoginPage";
+import { RegisterPage } from "./auth/pages/RegisterPage";
+
+const ChatLayout = lazy(() => import("./chat/layout/ChatLayout"));
+const ChatPage = lazy(() => import("./chat/pages/ChatPage"));
+const NoChatSelectedPage = lazy(
+  () => import("./chat/pages/NoChatSelectedPage")
+);
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/auth" element={<AuthLayout />}></Route>
+        <Route path="/auth" element={<AuthLayout />}>
+          <Route index element={<LoginPage />} />
+          <Route path="register" element={<RegisterPage />} />
+        </Route>
+
+        <Route
+          path="/chat"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <ChatLayout />
+            </Suspense>
+          }
+        >
+          <Route index element={<NoChatSelectedPage />} />
+          <Route path=":chatId" element={<ChatPage />} />
+        </Route>
 
         <Route path="/" element={<Navigate to="/auth" />} />
       </Routes>
