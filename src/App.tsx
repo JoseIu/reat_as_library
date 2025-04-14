087195fe004a7a3c09601cb490cad1801cea454e
Joselu <boxixz0991@gmail.com>
2025-04-14 23:50:58 +0200

    feat: add QueryClientProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { AppRouter } from "./AppRouter";
-
+const queryClient = new QueryClient();
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <AppRouter />
-    </>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
